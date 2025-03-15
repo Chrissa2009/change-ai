6f12f6cf9555f91f08531f4a516634d8d92ef250
Bryan Yue <yue_bryan123@hotmail.com>
2025-03-15 15:14:04 -0700

    Test GET surveys empty Azure Function

---
+++ api/function_app.py
@@ -0,0 +1,39 @@
+import azure.functions as func
+import logging
+import json
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+# TODO: Finish this Azure function GET "surveys" for listing saved survey names
+@app.route(route="surveys", methods=["GET"])
+def get_surveys(req: func.HttpRequest) -> func.HttpResponse:
+    request_body = None
+    try:
+        request_body = req.get_json()
+    except ValueError as e:
+        logging.error(f"GET surveys invalid JSON error: {e}")
+        return func.HttpResponse(
+            json.dumps({}),
+            mimetype='application/json',
+            status_code=400
+        )
+    logging.info(f"GET surveys request: {request_body}")
+    # START TODO: Read from Cosmos DB List of surveys
+    response_body = {
+        "surveys": []
+    }
+    # END TODO: Read from Cosmos DB
+    logging.info(f"GET surveys response: {response_body}")
+    return func.HttpResponse(
+        json.dumps(response_body),
+        mimetype='application/json',
+        status_code=200
+    )
+
+# TODO: Add GET "survey" to retrieve saved survey contents given a survey name in request body
+
+# TODO: Add PUT "survey" to create/update survey given a survey name
+
+# TODO: Add DELETE "survey" to delete survey given a survey name
+
+# TODO: Add GET "insight" for calculating and responding with aggregate insight including ROI and text-based insights/recommendations, given a survey name.
